b7fd0a97cffba52146a6daae3ecef7cd742431d2
bear (Mike Taylor) <bear@andyet.net>
2014-09-13 20:40:15 -0400

    refactoring for data storage how the Domain class works

--- domains.py
+++ domains.py
@@ -8,49 +8,80 @@ Walk thru a list of resources to extract possible IndieWeb domains from.
 """
 
 import os, sys
+import json
 import collections
-
 import requests
+
+from urlparse import urlparse, ParseResult
+from mf2py.parser import Parser
 
 class Domain(object):
     """An Indieweb Domain"""
 
-    def __init__(self, url):
-        self.url  = url
-        self.data = {}
+    def __init__(self, url, domainPath):
+        self.domainPath = domainPath
+        self.domain     = None
+        self.url        = None
+        self.domainFile = None
 
-    def loadJson(self, filename):
-        f = os.path.abspath(os.path.expanduser(filename))
-        if os.path.exists(f):
-            with open(f, 'r') as h:
-                self.data = json.load(h)
+        item = urlparse(url)
+        if item.scheme in ('http', 'https'):
+            self.domain = item.netloc
+            self.url    = url
         else:
-            self.data = {}
+            self.domain = item.path
+
+        self.dataFile = os.path.abspath(os.path.expanduser(os.path.join(self.domainPath, self.domain)))
+
+        if self.url is None:
+            if os.path.exists(self.dataFile):
+                with open(self.dataFile, 'r') as h:
+                    data     = json.load(h)
+                    self.url = data['url']
+            else:
+                self.url = 'http://%s' % self.domain
+
+    def refresh(self):
+        data = { 'url': self.url,
+                 'domain': self.domain,
+                 'mf2': {},
+               }
+        try:
+            r = requests.get(self.url, verify=False)
+            data['refresh'] = r.status_code
+            if r.status_code == requests.codes.ok:
+                if 'charset' in r.headers.get('content-type', ''):
+                    html = r.text
+                else:
+                    html = r.content
+                data['mf2'] = Parser(doc=html).to_dict()
+
+        except:
+            data['refresh'] = '%s' % sys.exc_info()[0]
+        with open(self.dataFile, 'w') as h:
+            h.write(json.dumps(data))
 
 class Domains(collections.OrderedDict):
     """A collection of Indieweb Domains"""
 
-    def __init__(self, domainPath, domainFile=None):
+    def __init__(self, dataPath, domainPath, domainFile):
         super(Domains, self).__init__()
+        self.dataPath   = dataPath
         self.domainPath = domainPath
-        self.domainFile = domainFile
+        self.domainFile = os.path.abspath(os.path.expanduser(os.path.join(self.dataPath, domainFile)))
 
-        if self.domainFile is not None:
-            self.load(self.domainFile)
+        self.load()
 
-    def load(self, domainFile):
-        self.domainFile = domainFile
-        f = os.path.abspath(os.path.expanduser(os.path.join(self.domainPath, self.domainFile)))
-        if os.path.exists(f):
-            with open(f, 'r') as h:
+    def load(self):
+        if os.path.exists(self.domainFile):
+            with open(self.domainFile, 'r') as h:
                 for line in h.readlines():
-                    url       = line.strip()
-                    domain    = Domain(url)
-                    self[url] = domain
-                    domain.loadJson(os.path.join(self.domainPath, url))
+                    domain = Domain(line.strip(), self.domainPath)
+                    if domain.domain is not None:
+                        self[domain.domain] = domain
 
     def store(self):
-        f = os.path.abspath(os.path.expanduser(os.path.join(self.domainPath, self.domainFile)))
-        with open(f, 'w') as h:
-            for url in self.keys():
-                h.write('%s\n' % url)
+        with open(self.domainFile, 'w') as h:
+            for key in self.keys():
+                domain = self[key]
+                h.write('%s\n' % domain.domain)
--- gather_domains.py
+++ gather_domains.py
@@ -13,18 +13,16 @@ import logging
 
 import requests
 from domains import Domains, Domain
-
-from urlparse import urlparse, ParseResult
 from mf2py.parser import Parser
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
 
 log = logging.getLogger('gather')
 
 
 def gather(cfg):
-    domains = Domains(cfg['domainPath'], cfg['domains'])
+    domains = Domains(cfg['dataPath'], cfg['domainPath'], cfg['domains'])
     log.info('%d domains loaded from datastore' % len(domains))
 
     r = requests.get(cfg['IRCPeople'], verify=False)
@@ -42,10 +40,13 @@ def gather(cfg):
                     for child in item['children']:
                         if 'properties' in child:
                             url = child['properties']['url'][0]
-                            if len(url) > 0 and url not in domains:
-                                log.info('%s not found in domain list' % url)
-                                n += 1
-                                domains[url] = Domain(url)
+                            if len(url) > 0:
+                                domain = Domain(url, cfg['domainPath'])
+                                if domain.domain not in domains:
+                                    log.info('%s not found in domain list' % domain.domain)
+                                    n += 1
+                                    domains[domain.domain] = domain
+                                    domain.refresh()
         # {   'alternates': [   {   'rel': u'meta',
         #                           'type': u'application/rdf+xml',
         #                           'url': u'/wiki/index.php?title=IRC_People&action=dublincore'},
@@ -95,25 +96,25 @@ def loadConfig(configFilename):
     filename = os.path.abspath(os.path.expanduser(configFilename))
     cfg      = {}
     if os.path.exists(filename):
-        cfg = json.load(open(filename, 'r'))
+        with open(filename, 'r') as h:
+            cfg = json.load(h)
     else:
         print('creating default configuration at %s' % filename)
         cwd = os.getcwd()
-        cfg['dataPath']   = os.path.join(cwd, 'data')
-        cfg['datastore']  = 'files'
-        cfg['domains']    = 'domains.dat'
-        cfg['mf2Path']    = os.path.join(cfg['dataPath'], 'mf2data')
-        cfg['domainPath'] = os.path.join(cfg['dataPath'], 'domains')
-        cfg['IRCPeople']  = 'http://indiewebcamp.com/IRC-people'
+        cfg['dataPath']  = os.path.join(cwd, 'data')
+        cfg['datastore'] = 'files'
+        cfg['domains']   = 'domains.dat'
+        cfg['domainPath']   = os.path.join(cfg['dataPath'], 'mf2data')
+        cfg['IRCPeople'] = 'http://indiewebcamp.com/IRC-people'
         with open(filename, 'w') as h:
             json.dump(cfg, h, indent=2)
 
         if not os.path.exists(cfg['dataPath']):
             print('creating dataPath %s' % cfg['dataPath'])
             os.mkdir(cfg['dataPath'])
-        if not os.path.exists(cfg['mf2Path']):
-            print('creating mf2Path %s' % cfg['mf2Path'])
-            os.mkdir(cfg['mf2Path'])
+        if not os.path.exists(cfg['domainPath']):
+            print('creating domainPath %s' % cfg['domainPath'])
+            os.mkdir(cfg['domainPath'])
 
     return cfg
 
@@ -123,7 +124,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='./indie-stats.cfg')
-    parser.add_argument('--echo',   default=False, action='store_true')
+    parser.add_argument('--echo',   default=True, action='store_true')
 
     args = parser.parse_args()
     cfg  = loadConfig(args.config)
